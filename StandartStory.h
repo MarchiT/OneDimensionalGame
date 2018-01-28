ebbca79f0f0045ef4c3533249facf5d205cb5526
Marina <mrtihova@gmail.com>
2018-01-28 13:06:08 +0200

    Chane entire standart story stucture

---
+++ StandartStory.h
@@ -0,0 +1,19 @@
+#ifndef STANDART_STORY_H_
+#define STANDART_STORY_H_
+
+#include "constants.h"
+
+struct StandartStory
+{
+
+	static char MAP_DEFAULT[NUM_LEDS];
+
+	static char MAP1[NUM_LEDS];
+
+	static char MAP2[NUM_LEDS];
+
+
+};
+
+
+#endif //STANDART_STORY_H_
