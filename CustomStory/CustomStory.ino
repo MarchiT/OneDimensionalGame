06d2b26ff147e7017ab758f6993bdf61cb68ca28
Marina <mrtihova@gmail.com>
2018-01-28 13:22:34 +0200

    Update examples to fit the new constructor

--- CustomStory/CustomStory.ino
+++ CustomStory/CustomStory.ino
@@ -9,17 +9,15 @@ char level1_map[NUM_LEDS] = { 0, 0, 0, 0, 'B', 'B', 'B', 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0 };
 
-OneDimentionalGame game;
+OneDimentionalGame game(CUSTOM);
 
 void setup() {
 	Serial.begin(115200);
 	Serial.println("Serial initialized");
-
 	Serial2.begin(9600);
 
-  game.create_own_story();
-  game.add_level(20, level1_map);
-  game.add_props(0, 5, 10);
+	game.add_level(20, level1_map);
+	game.add_props(0, 5, 10);
 }
 
 void loop() {
--- StandartStory/StandartStory.ino
+++ StandartStory/StandartStory.ino
@@ -5,10 +5,7 @@ OneDimentionalGame game;
 void setup() {
   Serial.begin(115200);
   Serial.println("Serial initialized");
-
   Serial2.begin(9600);
-
-  game.create_default_story();
 }
 
 void loop() {
