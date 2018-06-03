0533c903337a7fdb65460fea92ef51204995f2a0
MarchiT <mrtihova@gmail.com>
2018-06-03 17:57:02 +0300

    Add Standard Maps

---
+++ StandardMaps.h
@@ -0,0 +1,25 @@
+#ifndef STANDARD_MAPS_H_
+#define STANDARD_MAPS_H_
+
+#include"constants/specifications.h"
+
+struct StandardMaps
+{
+
+	static char MAP_DEFAULT[NUM_LEDS];
+
+	static char MAP_BLANK[NUM_LEDS];
+
+	static char MAP_TEST[NUM_LEDS];
+
+	static char MAP_GREEN_PINK[NUM_LEDS];
+
+	static char MAP_BLUE_TO_GREEN[NUM_LEDS];
+
+	static char MAP_STOPLIGHT[NUM_LEDS];
+
+	static char MAP268[268];
+};
+
+
+#endif //STANDARD_MAPS_H_
